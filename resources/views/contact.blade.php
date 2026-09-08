0f01468dc8509838e4b8cd0bba025db7c1a176d8
Xime <ximena.ruiz@cceo.com.mx>
2026-09-08 17:21:38 -0600

    feat (Layout):✨implement layout inheritance with extends and sections in contact2 view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,30 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('master')
 
-
+@section('contect')
     <h1>Contact 1</h1>
     <p>{{ $name }}</p>
 
-    @if($name != "Ximena")
+    @if($name != "Andres")
         Tu nombre no es Andres
     @else
-        <h3>tu nombre es Ximena</h3>
+        <h2>Tu nombre es Andres</h2>
     @endif
 
-
     <ul>
-    @foreach ([1,2,3,4,5] as $item)
-        <li>{{ $item }}</li>
-    @endforeach
+        @foreach ([1,2,3,4,5] as $item)
+            <li>{{ $item }}</li>
+        @endforeach
     </ul>
-
-</body>
-</html>
+@endsection
