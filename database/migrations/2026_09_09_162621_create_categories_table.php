50d3f337cb9d08616e4d116a25f2627ad5f1a20b
Xime <ximena.ruiz@cceo.com.mx>
2026-09-09 10:40:37 -0600

    feat (Migracion-Post):✨update categories table migration with description, content, image, and posted fields

--- database/migrations/2026_09_09_162621_create_categories_table.php
+++ database/migrations/2026_09_09_162621_create_categories_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title',500);
-            $table->string('slug',500);
+            $table->string('title', 500);
+            $table->string('slug', 500);
+            $table->string('description')->nullable();
+            $table->string('content')->nullable();
+            $table->string('image')->nullable();
+            $table->enum('posted', ['yes', 'not'])->default('not');
+            $table->timestamps();
         });
     }
 
