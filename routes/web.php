1b1603c1ff8973291ba0d7efa638c457c911332f
Xime <ximena.ruiz@cceo.com.mx>
2026-09-09 09:54:34 -0600

    feat (compact):✨configure controller and pass posts array to contact view

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\PrimerControlador;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrimerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', [PrimerControlador::class, 'index']);
-//Route::resource('post', PrimerControlador::class);
+Route::get('test', [PrimerController::class, 'index']);
+//Route::resource('post', PrimerController::class);
