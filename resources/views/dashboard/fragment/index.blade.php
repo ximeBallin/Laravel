288695aece791adc2df25b51f0e3e690dfd63d71
Xime <ximena.ruiz@cceo.com.mx>
2026-09-10 15:19:31 -0600

    feat (Opciones-CRUD):✨open create link in new tab and include id, category title, and action options in index table

--- resources/views/dashboard/fragment/index.blade.php
+++ resources/views/dashboard/fragment/index.blade.php
@@ -1,20 +1,34 @@
 @extends('dashboard.master')
 
 @section('content')
+
+   <a href="{{route('post.create')}}" target="_blank" >Create</a>
+
     <table>
         <thead>
         <tr>
-            <th>Title</th>
-            <th>Posted</th>
-            <th>Category</th>
+            <td>id</td>
+            <td>title</td>
+            <td>Posted</td>
+            <td>Category</td>
+            <td>Options</td>
+
+
         </tr>
         </thead>
         <tbody>
         @foreach($posts as $p)
             <tr>
+                <td>{{ $p->id }}</td>
                 <td>{{ $p->title }}</td>
                 <td>{{ $p->posted }}</td>
-                <td>{{ $p->category }}</td>
+                <td>{{ $p->category->title }}</td>
+                <td>
+                    <a href="{{route('post.edit',$p->id)}}">Edit</a>
+                    <a href="{{route('post.show',$p->id)}}">Show</a>
+
+                </td>
+
             </tr>
         @endforeach
         </tbody>
