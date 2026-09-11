514ae2daaa60dc8160eb8f9d64b71883f749a99b
Xime <ximena.ruiz@cceo.com.mx>
2026-09-11 11:35:44 -0600

    feat (Rutas-Repaso):✨pdate web routes to define namespaced dashboard resource endpoints for posts and categories

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Dashboard\CategoryController;
+use App\Http\Controllers\Dashboard\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-
-Route::resource('category', App\Http\Controllers\Dashboard\CategoryController::class);
+Route::resource('dashboard/post', PostController::class);
+Route::resource('dashboard/category', App\Http\Controllers\Dashboard\CategoryController::class);
