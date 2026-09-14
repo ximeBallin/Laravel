cbd866577731950960328b68b4a65696b9cfc364
Xime <ximena.ruiz@cceo.com.mx>
2026-09-14 16:43:01 -0600

    feat (Componentes-Anonimos):✨implement anonymous card component in blog index loop

--- resources/views/dashboard/blog/index.blade.php
+++ resources/views/dashboard/blog/index.blade.php
@@ -1,13 +1,4 @@
 @extends('dashboard.master')
-
 @section('content')
-    <h1>Listado del Blog</h1>
-    @foreach($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
-            <p>{{ $post->content }}</p>
-        </div>
-    @endforeach
-
-    {{ $posts->links() }}
+    <h1>ESTOY EN EL BLOG</h1>
 @endsection
