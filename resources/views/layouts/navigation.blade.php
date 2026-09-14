ff0bd7a206c5a481b69d2d612080321fbb0da4d4
Xime <ximena.ruiz@cceo.com.mx>
2026-09-14 11:02:32 -0600

    feat (Configurar-Layout):✨Add category navigation link and tab to the dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,10 @@
                     <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.*')">
                         {{ __('Post') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.*')">
+                        {{ __('Category') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -77,6 +81,10 @@
 
             <x-responsive-nav-link :href="route('post.index')" :active="request()->routeIs('post.*')">
                 {{ __('Post') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('category.index')" :active="request()->routeIs('category.*')">
+                {{ __('Category') }}
             </x-responsive-nav-link>
         </div>
 
