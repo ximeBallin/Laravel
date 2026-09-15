6d56ae1fc66240385ac70d1da48ade567be2ee07
Xime <ximena.ruiz@cceo.com.mx>
2026-09-14 20:16:21 -0600

    feat (Iconografia):✨install and configure Material Design Icons (mdi/font) for Oruga UI components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,13 @@ import { createApp } from 'vue';
 import App from './vue/App.vue';
 import Oruga from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga.css';
+import '@mdi/font/css/materialdesignicons.min.css';
 
 const app = createApp(App);
 
 app.use(Oruga);
 app.mount('#app');
+
+app.use(Oruga, {
+    iconPack: 'mdi',
+});
