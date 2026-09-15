135b2cb7f7e4e1a64dda57bcbed9221961589789
Xime <ximena.ruiz@cceo.com.mx>
2026-09-14 20:03:47 -0600

    feat (Oruga-UI):✨install and configure Oruga UI component library in Vue 3 app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import App from './components/App.vue'; // O tu componente principal
+import App from './vue/App.vue';
+import Oruga from '@oruga-ui/oruga-next';
+import '@oruga-ui/oruga-next/dist/oruga.css';
 
 const app = createApp(App);
+
+app.use(Oruga);
 app.mount('#app');
