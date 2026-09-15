42446d9702e8dc0450c42b06aa7e5887615ff3c6
Xime <ximena.ruiz@cceo.com.mx>
2026-09-14 20:01:03 -0600

    feat (Primeros-Pasos):✨reate App.vue and List.vue components structure for initial Vue setup

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Principal</h1>
+        <!-- Renderizamos el componente List importado -->
+        <List />
+    </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+
+export default {
+    components: {
+        List
+    }
+}
+</script>
